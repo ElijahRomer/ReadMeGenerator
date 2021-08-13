d97bfa83516032a02440e4cb6a32c49ab284fe0d
Elijah Romer <romere88@gmail.com>
2021-08-13 15:13:34 -0500

    Coded basic app questions and markdown template

--- index.js
+++ index.js
@@ -1,47 +1,131 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
+const generateMarkdown = require (`./utils/generateMarkdown`);
 
 // TODO: Create an array of questions for user input
 const questions = [
   { type: "input",
+    name: "fileName",
+    message: "Please enter the name of your readme file excluding the extension",
+    default: "test"
+  },
+  { type: "input",
     name: "title",
-    message: "What is the title of your application?"
+    message: "What is the title of your application?",
+    default: "Awesome Application"
   },
   { type: "input",
     name: "description",
-    message: "Please enter a detailed description of your application."
+    message: "Please enter a detailed description of your application.",
+    default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rerum quod, animi unde dolor veniam voluptates accusamus magni eaque omnis odit libero earum deserunt accusantium molestiae ipsa nostrum quidem ut."
   },
   { type: "input",
     name: "installation",
-    message: "Please enter Installation instructions for your application"
+    message: "Please enter Installation instructions for your application.",
+    default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rerum quod, animi unde dolor veniam voluptates accusamus magni eaque omnis odit libero earum deserunt accusantium molestiae ipsa nostrum quidem ut."
   },
   { type: "input",
     name: "usage",
-    message: "Please enter usage information for your application"
+    message: "Please enter usage information for your application.",
+    default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rerum quod, animi unde dolor veniam voluptates accusamus magni eaque omnis odit libero earum deserunt accusantium molestiae ipsa nostrum quidem ut."
   },
   { type: "input",
     name: "contribution",
-    message: "Please guidelines for individuals looking to contribute to your application"
+    message: "Please enter guidelines for individuals looking to contribute to your application.",
+    default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rerum quod, animi unde dolor veniam voluptates accusamus magni eaque omnis odit libero earum deserunt accusantium molestiae ipsa nostrum quidem ut."
   },
   { type: "input",
-    name: "Table of Contents",
-    message: "Please enter Installation instructions for your application"
+    name: "testing",
+    message: "Please enter instructions for users to test your application.",
+    default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rerum quod, animi unde dolor veniam voluptates accusamus magni eaque omnis odit libero earum deserunt accusantium molestiae ipsa nostrum quidem ut."
+  },
+  { type: "input",
+    name: "github",
+    message: "Please enter your github username to add to the additional questions section.",
+    default: "ElijahRomer"
+  },
+  { type: "list",
+    name: "license",
+    message: "Please select the usage license for this application:",
+    choices: [
+      `Apache 2.0`, 
+      new inquirer.Separator(`******`), 
+      `GNU GPL v3`, 
+      new inquirer.Separator(`******`), 
+      `GNU GPL v2`, 
+      new inquirer.Separator(`******`), 
+      `ISC`, 
+      new inquirer.Separator(`******`), 
+      `MIT`, 
+      new inquirer.Separator(`******`), 
+      `MPL 2.0`, 
+      new inquirer.Separator(`******`), 
+      `The Unlicense`, 
+      new inquirer.Separator(`******`)
+    ]
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  let markdown = generateMarkdown(data);
+  fs.writeFile(`${fileName}.md`, markdown, (err) => {
+    if (err) {
+      console.log(`something went wrong line 67`, err);
+    }
+    else {console.log(`Data Written`)}
+  })
+}
 
 // TODO: Create a function to initialize app
 function init() {
 inquirer.prompt(questions)
-.then((answers) =>
-  console.log(answers)
-  )
-
-
-    }
+.then((answers) => {
+  console.log(`line 60`, answers)
+  console.log(`line 61`, answers.fileName)
+  writeToFile(answers.fileName, answers)
+})}
 
 // Function call to initialize app
 init();
+
+
+//code out question logic
+  //code out license multiple choice logic
+  //make the license a clickable link to the corresponding page on https://opensource.org/licenses
+
+  //code out Further Questions section questions
+    //auto generate working link to github
+    //add email and question for how to reach me
+
+
+
+
+//code out writing to file logic
+    //Title
+
+    //Description
+      //Should be able to just insert the raw text from inquirer answer object
+
+    //Table of Contents
+      //Will be hardcoded to correspond with each of the Installation, Usage, License, Contributing, Tests, and Questions sections
+      //each entry must be a markdown link to corresponding section
+
+    //Installation
+      //Should be able to just insert the raw text from inquirer answer object
+
+    //Usage
+      //Should be able to just insert the raw text from inquirer answer object
+
+    //License
+      //badge icon for license must appears near top in the markdown
+      //license explanation and insertion logic
+
+    //Contributing
+
+    //Tests
+
+    //Questions
+    
+    
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,145 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  switch (license) {
+    case `Apache 2.0`:
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    case `GNU GPL v3`:
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+
+    case `GNU GPL v2`:
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+
+    case `ISC`:
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+
+    case `MIT`:
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+    case `MPL 2.0`:
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+
+    case `The Unlicense`:
+      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  }  
+}
+
+function licenseNameVerbose(license) {
+  if (!license) {
+    return "";
+  }
+  switch (license) {
+    case "Apache 2.0":
+      return "Apache 2.0";
+
+    case `GNU GPL v3`:
+      return `GNU General Public License (GPL) v3`
+
+    case `GNU GPL v2`:
+      return `GNU General Public License (GPL) v2`
+
+    case `ISC`:
+      return `Internet Services Consortium License (ISC)`
+
+    case `MIT`:
+      return `The Massachusets Institute of Technology License (MIT)`
+
+    case `MPL 2.0`:
+      return `Mozilla Public License (MPL) 2.0`
+
+    case `The Unlicense`:
+      return `The Unlicense`
+  }  
+}
+
+function licenseLegalTerms(license) {
+  if (!license) {
+    return "";
+  }
+  switch (license) {
+    case `Apache 2.0`:
+      return `https://www.apache.org/licenses/LICENSE-2.0.txt`;
+
+    case `GNU GPL v3`:
+      return `https://www.gnu.org/licenses/gpl-3.0.en.html`
+
+    case `GNU GPL v2`:
+      return `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`
+
+    case `ISC`:
+      return `https://opensource.org/licenses/ISC`
+
+    case `MIT`:
+      return `https://www.mit.edu/~amini/LICENSE.md`
+
+    case `MPL 2.0`:
+      return `https://www.mozilla.org/media/MPL/2.0/index.48a3fe23ed13.txt`
+
+    case `The Unlicense`:
+      return `https://unlicense.org/`
+  }  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) return "";
+  return `
+
+  This application is licensed under ${license}.
+  
+  Click the license badge below for more information and usage guidelines:
+
+  ${renderLicenseBadge(license)}
+  
+  Click [here](${licenseLegalTerms(license)}
+    "${license} Full Terms and Conditions") to view the full terms and conditions text of ${license}.`
+}
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ---
+  ## Table of Contents
+  1. [Installation](#installation)
+  1. [License Information](#license-information)
+  1. [License Information](#license-information)
+  1. [License Information](#license-information)
+
+  ---
+  ## Installation
+  ${data.installation}
+
+  ---
+  ## Usage
+  ${data.usage}
+
+  ---
+  ## License Information
+  ${renderLicenseSection(data.license)}
+
+  ---
+  ## Contributing
+  ${data.contribution}
+
+  ---
+  ## Tests
+  ${data.testing}
+
+  ---
+  ## Questions
+  ${data.github}
+
 `;
 }
 
