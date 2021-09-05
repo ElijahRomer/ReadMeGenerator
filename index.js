c90bea6a0008168f66ee2bd1f123ffe9e02c1426
Elijah Romer <romere88@gmail.com>
2021-09-05 17:48:26 -0500

    added default value to contribution section

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
-const generateMarkdown = require (`./utils/generateMarkdown`);
+const generateMarkdown = require(`./utils/generateMarkdown`);
 
 const possibleSections = [
   `Installation`,
@@ -16,132 +16,151 @@ const possibleSections = [
 
 // TODO: Create an array of questions for user input
 const questions = [
-  { type: "input",
+  {
+    type: "input",
     name: "writeFileAbsolutePath",
     message: "Please copy and paste the ABSOLUTE path to the directory in which you want your README to appear. Hit enter to choose the current directory of the terminal.",
     default: "./"
   },
-  { type: "input",
+  {
+    type: "input",
     name: "fileName",
     message: "Please enter the name for your readme file, NOT INCLUDING the file extension",
     default: "README"
   },
-  { type: "input",
-  name: "title",
-  message: "What is the title of your application?",
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your application?",
   },
-  { type: "editor",
-  name: "description",
-  message: "Please enter a short description of your application.",
+  {
+    type: "editor",
+    name: "description",
+    message: "Please enter a short description of your application.",
   },
-  { type: "input",
+  {
+    type: "input",
     name: "imagePath",
     message: "If you would like to add a screenshot of your application, please copy and paste the URL or the RELATIVE filepath from your application's root directory here. Otherwise, hit enter to skip.",
   },
-      { type: "input",
-        name: "imageAltText",
-        message: "Enter the alt text for your image",
-        when: (answers) => answers.imagePath !== ""
-      },
-      { type: "input",
-        name: "imageHoverText",
-        message: "Enter the text to appear when image is hovered over.",
-        when: (answers) => answers.imagePath !== ""
-      },
-  { type: "checkbox",
+  {
+    type: "input",
+    name: "imageAltText",
+    message: "Enter the alt text for your image",
+    when: (answers) => answers.imagePath !== ""
+  },
+  {
+    type: "input",
+    name: "imageHoverText",
+    message: "Enter the text to appear when image is hovered over.",
+    when: (answers) => answers.imagePath !== ""
+  },
+  {
+    type: "checkbox",
     name: "includeSections",
     message: "Please select which other sections you would like your readme to include. (ALL are selected by default)",
     choices: possibleSections,
     default: possibleSections
   },
-  { type: "editor",
+  {
+    type: "editor",
     name: "Installation",
     message: "Please enter Installation instructions for your application.",
     when: (answers) => answers.includeSections.includes("Installation")
   },
-  { type: "editor",
+  {
+    type: "editor",
     name: "Usage",
     message: "Please enter a detailed description of how to use your application.",
     when: (answers) => answers.includeSections.includes("Usage")
   },
-  { type: "editor",
+  {
+    type: "editor",
     name: "Contribution",
     message: "Please enter guidelines for individuals looking to contribute to your application.",
+    default: "Thank you for considering contributing to this project. If you would like to contribute, feel free to fork the repository, add your functionality or bugfix, and submit a pull request. I will review the changes and contact you with any questions or concerns.",
     when: (answers) => answers.includeSections.includes("Contribution")
   },
-  { type: "editor",
+  {
+    type: "editor",
     name: "Testing",
     message: "Please enter instructions for users to test your application.",
     when: (answers) => answers.includeSections.includes("Testing")
   },
-  { type: "editor",
+  {
+    type: "editor",
     name: "Questions",
     message: "Please enter guidelines for getting in contact with you if users have further questions. \n***Do not include specific contact methods, as those will be in the next prompt.",
     default: "Please do not hesitate to reach out with any questions you may have. I can be reached in the following ways:",
     when: (answers) => answers.includeSections.includes("Questions")
   },
-  { type: "checkbox",
+  {
+    type: "checkbox",
     name: "ContactMethods",
     message: "Please select the ways in which users may contact you",
     default: [
       `Email`,
-      `GitHub`, 
+      `GitHub`,
       `Twitter`
     ],
     choices: [
       `Email`,
-      `GitHub`, 
+      `GitHub`,
       `Twitter`
     ],
     when: (answers) => answers.includeSections.includes("Questions")
   },
-      { type: "input",
-        name: "GitHub",
-        message: "Please enter your GitHub username to populate to contact methods.",
-        when: (answers) => {
-          if(answers.includeSections.includes(`Questions`) && answers.ContactMethods.includes(`GitHub`)){
-            return true;
-          }
-        }
-      },
-      { type: "input",
-        name: "Email",
-        message: "Please enter your Email to populate to contact methods.",
-        when: (answers) => {
-          if(answers.includeSections.includes(`Questions`) && answers.ContactMethods.includes(`Email`)){
-            return true;
-          }
-        }
-      },
-      { type: "input",
-        name: "Twitter",
-        message: "Please enter your Twitter handle to populate to contact methods.",
-        when: (answers) => {
-          if(answers.includeSections.includes(`Questions`) && answers.ContactMethods.includes(`Twitter`)){
-            return true;
-          }
-        }
-      },
-  { type: "list",
+  {
+    type: "input",
+    name: "GitHub",
+    message: "Please enter your GitHub username to populate to contact methods.",
+    when: (answers) => {
+      if (answers.includeSections.includes(`Questions`) && answers.ContactMethods.includes(`GitHub`)) {
+        return true;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "Email",
+    message: "Please enter your Email to populate to contact methods.",
+    when: (answers) => {
+      if (answers.includeSections.includes(`Questions`) && answers.ContactMethods.includes(`Email`)) {
+        return true;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "Twitter",
+    message: "Please enter your Twitter handle to populate to contact methods.",
+    when: (answers) => {
+      if (answers.includeSections.includes(`Questions`) && answers.ContactMethods.includes(`Twitter`)) {
+        return true;
+      }
+    }
+  },
+  {
+    type: "list",
     name: "License",
     message: "Please select the usage license for this application, or hit enter to skip",
     pageSize: 16,
     choices: [
       ``,
       new inquirer.Separator(`------`),
-      `Apache 2.0`, 
-      new inquirer.Separator(`------`), 
-      `GNU GPL v3`, 
-      new inquirer.Separator(`------`), 
-      `GNU GPL v2`, 
-      new inquirer.Separator(`------`), 
-      `ISC`, 
-      new inquirer.Separator(`------`), 
-      `MIT`, 
-      new inquirer.Separator(`------`), 
-      `MPL 2.0`, 
-      new inquirer.Separator(`------`), 
-      `The Unlicense`, 
+      `Apache 2.0`,
+      new inquirer.Separator(`------`),
+      `GNU GPL v3`,
+      new inquirer.Separator(`------`),
+      `GNU GPL v2`,
+      new inquirer.Separator(`------`),
+      `ISC`,
+      new inquirer.Separator(`------`),
+      `MIT`,
+      new inquirer.Separator(`------`),
+      `MPL 2.0`,
+      new inquirer.Separator(`------`),
+      `The Unlicense`,
       new inquirer.Separator(`------`)
     ],
     when: (answers) => answers.includeSections.includes("License")
@@ -163,10 +182,11 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt(questions)
-.then((answers) => {
-  writeToFile(`${answers.writeFileAbsolutePath}/${answers.fileName}`, answers)
-})}
+  inquirer.prompt(questions)
+    .then((answers) => {
+      writeToFile(`${answers.writeFileAbsolutePath}/${answers.fileName}`, answers)
+    })
+}
 
 // Function call to initialize app
 
@@ -175,13 +195,13 @@ inquirer.prompt(questions)
 let terminalArgs = process.argv;
 
 let extractedTerminalArgs = terminalArgs.map((arg) => {
-  if (arg.includes(`node.exe`)){
+  if (arg.includes(`node.exe`)) {
     return arg.substr(arg.indexOf(`node.exe`), 8);
   }
-  if (arg.includes(`index`)){
+  if (arg.includes(`index`)) {
     return arg.substr(arg.indexOf(`index`), 5);
   }
-return false;
+  return false;
 })
 
 let checkTerminalArgs = (extractedTerminalArgs) => {
@@ -189,7 +209,7 @@ let checkTerminalArgs = (extractedTerminalArgs) => {
     console.log(`Initializing README Generator...`);
     return;
   }
-  if (extractedTerminalArgs.includes(`node.exe`) && extractedTerminalArgs.includes(`index`)){
+  if (extractedTerminalArgs.includes(`node.exe`) && extractedTerminalArgs.includes(`index`)) {
     console.log(`Initializing README Generator...`);
     init();
   }
@@ -199,4 +219,4 @@ checkTerminalArgs(extractedTerminalArgs);
 
 exports.init = init;
 
-    
+
